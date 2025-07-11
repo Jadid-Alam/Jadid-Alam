6ed6949ab2afd2730e76506d8bdcc995d922d0a3
Jadid-Alam <153989441+Jadid-Alam@users.noreply.github.com>
2025-07-11 20:37:04 +0100

    fix: not all languages have an image

--- repo-info-script/script.js
+++ repo-info-script/script.js
@@ -115,7 +115,7 @@ const filteredLanguages = Object.fromEntries(
 );
 
 const languageStats = Object.entries(filteredLanguages)
-  .map(([language, value]) => `* <img src="https://skillicons.dev/icons?i=${language.toLowerCase()}"/>: ${Math.round(value*1000/totalCount)/10}%`)
+  .map(([language, value]) => `* **language**: ${Math.round(value*1000/totalCount)/10}%`)
   .join('\n');
 
 await fs.writeFile(
